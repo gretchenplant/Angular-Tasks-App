704c5bebbb31414e920b5ce8e86c80db499cb28e
gretchenplant <gretchen.e.plant@gmail.com>
2025-02-10 14:45:30 -0500

    tidied up users list to for loop instead of manually assigning

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 
 @Component({
@@ -11,20 +15,19 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class UserComponent {
 
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string; 
+
+  @Input ({required: true}) user!: User;
   @Output() select = new EventEmitter();
 
 
 get imagePath() {
-  return 'assets/users/' + this.avatar;
+  return 'assets/users/' + this.user.avatar;
 }
  
  
  onSelectUser() {
   
-  this.select.emit(this.id);
+  this.select.emit(this.user.id);
 
   }
 }
